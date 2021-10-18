9fccc1f74c56cb28a85f613ea8570f293997d12e
王明扬 <wangmingyang@sunline.cn>
2021-10-18 23:17:08 +0800

    需求三

--- src/main/java/com/wmy/cosmetic/entity/Employee.java
+++ src/main/java/com/wmy/cosmetic/entity/Employee.java
@@ -17,7 +17,6 @@ public class Employee {
     private int id;
     private String uuid;//职员编号
     private String username;//账号
-    private String username1;//账号
     private String password;//密码
     private String name;//姓名
     private String sex;//性别
